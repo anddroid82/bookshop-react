72851545e04313f64578f310da2f1f5a85184a7e
anddroid82 <anddroid82@gmail.com>
2023-10-03 15:21:59 +0200

    v1

--- src/book/Book.js
+++ src/book/Book.js
@@ -1,16 +1,36 @@
+import { useRef, useState } from 'react';
 import './Book.css'
 
 function Book({ book }) {
+
+    const [edited, setEdited] = useState(false);
+    const inputRef = useRef(null);
+    const [newPrice,setNewPrice]=useState(book.price);
+
+    const save = (event) => {
+        console.log(newPrice);
+    }
+
     return (
         <div className="card book">
-            <img className="card-img-top" src={book.image} alt={book.title} />
+            <img className="card-img-top bookcover" src={book.image} alt={book.title} />
             <div className="card-body d-flex flex-column">
                 <h5 className="card-title">{book.title}</h5>
                 <p className="card-text">{book.summary}</p>
                 <p className="card-text"><strong>Szerző: </strong>{book.authors[0].name}</p>
                 <div className='mt-auto'>
-                    <p><strong>Ár: </strong>{book.price}</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <div>
+                        <strong>Ár: </strong>
+                        {edited ?
+                            (<div className="input-group">
+                                <input type="text" className="form-control" value={newPrice} onChange={(e)=>setNewPrice(e.target.value)} />
+                                <div className="input-group-append">
+                                    <i className="bi bi-arrow-counterclockwise" onClick={()=>setEdited(!edited)}></i>
+                                    <i className="bi bi-check" onClick={(event)=>{setEdited(!edited);save(event)}}></i>
+                                </div>
+                            </div>) :
+                            (<span>{book.price} <i className="bi bi-pen" onClick={() => setEdited(!edited)}></i></span>)}
+                    </div>
                 </div>
             </div>
 
